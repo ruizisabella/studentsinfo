a11c195b5b2a9b23b907a58d1cb0dec47bd2db00
Fredy Ballesteros <33910699+fredyballest@users.noreply.github.com>
2024-10-14 14:29:56 -0500

    Getting rid of base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/studentsinfo/',
   plugins: [react()],
   preview: {
     port: 3000,
