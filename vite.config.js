618d4e66a2ca0dc0859dc817e7307adc1b03f08f
Fredy Ballesteros <33910699+fredyballest@users.noreply.github.com>
2024-10-14 13:23:30 -0500

    Adding base to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/studentsinfo/',
   plugins: [react()],
   preview: {
     port: 3000,
